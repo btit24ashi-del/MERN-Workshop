e168f3cd29fdc0245fe39ad3a8f6f0ffa6af4f98
btit24ashi-del <btit24a.shi@ismt.edu.np>
2026-04-03 12:02:28 +0545

    Features

---
+++ src/components/Features.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Features = () => {
+  return (
+    <section>
+        <div>
+            <h2>Features</h2>
+        </div>
+        <div>
+            <div>
+                <h3>Real Time Response</h3>
+                <p>Get instant answers to your questions with our AI-powered chatbot.</p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Features
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
         <div className='items-center justify-center flex flex-col gap-10 py-20'>
         <h1 className='text-6xl font-bold'>Solve your Queries with ISMT Chat</h1>
         <p className= 'text-3xl font-light'>Get instant answers to your questions with our AI-powered chatbot.</p>
-        <button  className='bg-black text-white rounded-3xl px-7 py-2 hover:bg-white hover:text-black cursor-grabbing h-50'>Get Started</button>
+        <button  className='bg-black text-white rounded-3xl px-7 py-2 hover:bg-white hover:text-black cursor-grabbing h-20'>Get Started</button>
     </div></section>
   )
 }
